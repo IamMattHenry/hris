a775ea41a31ea044ce9ca239cbe4c030aec39600
Christian Ihao Ancog <137300859+XYZygarde@users.noreply.github.com>
2026-02-27 15:33:42 +0800

    fixed invalid auth redirecting

--- src/app/(auth)/login_hr/loginform.tsx
+++ src/app/(auth)/login_hr/loginform.tsx
@@ -79,6 +79,8 @@ export default function LoginForm() {
         if (result.message === "Invalid credentials") {
           setErrorMessage("Incorrect username or password.");
           alert("Incorrect username or password.");
+          window.location.href = "/login_hr";
+          return;
         } else {
           setErrorMessage(result.message || "Login failed. Please try again.");
           alert(result.message || "Login failed. Please try again.");
